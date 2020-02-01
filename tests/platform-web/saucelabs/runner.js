9726b142252c2d2a3ae9b42a48f4116c5ad2bd05
James Cahill <jcahill773@gmail.com>
2020-02-01 18:22:08 +0000

    Working test runner with automatic cleanup

--- tests/platform-web/saucelabs/runner.js
+++ tests/platform-web/saucelabs/runner.js
@@ -2,18 +2,21 @@ const opts = require("./opts");
 
 const { parallel } = require("async");
 
+const axios = require("axios").default;
+
 const runner = async () => {
   const browsers = await opts.getBrowsers();
 
   const out = [];
 
   browsers.forEach((value, index) => {
-    out.push(function() {
+    out.push(async function() {
       console.log(
         `Browser #${index} - ${value.browserName}@${value.version} under ${value.platform}`
       );
       const driver = opts.makeDriver(value);
-      opts.run(driver);
+      await opts.run(driver);
+      console.log(`ARR DONE - ${index}`);
     });
   });
 
@@ -25,7 +28,33 @@ console.log("Waiting 30 seconds for the proxy to start...");
 setTimeout(
   () =>
     runner().then(arr => {
-      parallel(arr, () => console.log("done"));
+      parallel(arr, async () => {
+        // All tests completed, cleanup time.
+
+        // Use axios / saucelabs API to shutdown the SauceConnect proxy
+        (
+          await axios.get(
+            `https://eu-central-1.saucelabs.com/rest/v1/${process.env.SAUCE_USERNAME}/tunnels`,
+            {
+              auth: {
+                username: process.env.SAUCE_USERNAME,
+                password: process.env.SAUCE_ACCESS_KEY
+              }
+            }
+          )
+        ).data.forEach(async tunnel => {
+          console.log("Shutting down Tunnel: " + tunnel);
+          await axios.delete(
+            `https://eu-central-1.saucelabs.com/rest/v1/${process.env.SAUCE_USERNAME}/tunnels/${tunnel}`,
+            {
+              auth: {
+                username: process.env.SAUCE_USERNAME,
+                password: process.env.SAUCE_ACCESS_KEY
+              }
+            }
+          );
+        });
+      });
     }),
   30000
 );
