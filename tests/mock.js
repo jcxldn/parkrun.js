27bfc94e02820e8305f7cee934c601fa713577d2
James Cahill <jcahill773@gmail.com>
2020-05-21 10:18:30 +0100

    Run npm install, add RosterVolunteer nock tests

--- tests/mock.js
+++ tests/mock.js
@@ -8,14 +8,12 @@ const UserPassError = require("../src/errors/ParkrunUserPassError");
 chai.should();
 describe("Mock", () => {
   describe("Authentication", () => {
-    it("HTTP 200 OK, no response data.", done => {
-      nock("https://api.parkrun.com")
-        .post("/user_auth.php")
-        .reply(200);
+    it("HTTP 200 OK, no response data.", (done) => {
+      nock("https://api.parkrun.com").post("/user_auth.php").reply(200);
 
       Parkrun.authSync("user", "pass")
         .then(() => done(new Error()))
-        .catch(err => {
+        .catch((err) => {
           if (err instanceof ValidationError) {
             done();
           } else {
@@ -24,56 +22,153 @@ describe("Mock", () => {
         });
     });
 
-    it("HTTP 200 OK, valid response data.", done => {
-      nock("https://api.parkrun.com")
-        .post("/user_auth.php")
-        .reply(200, {
+    it("HTTP 200 OK, valid response data.", (done) => {
+      nock("https://api.parkrun.com").post("/user_auth.php").reply(200, {
+        access_token: "access_token",
+        expires_in: "7200",
+        token_type: "bearer",
+        scope: "app",
+        refresh_token: "refresh_token",
+      });
+
+      Parkrun.authSync("user", "pass")
+        .then(() => done())
+        .catch((err) => done(err));
+    });
+
+    // 401s are calculated based on response code only, so currently response data does not matter.
+    it("HTTP 401 Unauthorized, no response data", (done) => {
+      nock("https://api.parkrun.com").post("/user_auth.php").reply(400);
+
+      Parkrun.authSync("user", "pass")
+        .then(() => done(new Error()))
+        .catch((err) => {
+          if (err instanceof UserPassError) {
+            done();
+          } else {
+            done(err);
+          }
+        });
+    });
+
+    it("HTTP 401 Unauthorized, valid response data", (done) => {
+      nock("https://api.parkrun.com").post("/user_auth.php").reply(400, {
+        error: "invalid_grant",
+        error_description: "Invalid username and password combination",
+      });
+
+      Parkrun.authSync("user", "pass")
+        .then(() => done(new Error()))
+        .catch((err) => {
+          if (err instanceof UserPassError) {
+            done();
+          } else {
+            done(err);
+          }
+        });
+    });
+  });
+
+  describe("Event", () => {
+    describe("RosterVolunteer", () => {
+      let roster = null;
+      let date = null;
+      before((done) => {
+        date = new Date();
+
+        // Get a fake instance
+        nock("https://api.parkrun.com").post("/user_auth.php").reply(200, {
           access_token: "access_token",
           expires_in: "7200",
           token_type: "bearer",
           scope: "app",
-          refresh_token: "refresh_token"
+          refresh_token: "refresh_token",
         });
 
-      Parkrun.authSync("user", "pass")
-        .then(() => done())
-        .catch(err => done(err));
-    });
+        Parkrun.authSync("user", "pass").then((parkrun) => {
+          // Get a fake roster
+          nock("https://api.parkrun.com")
+            .get(
+              "/v1/events/0/rosters?expandedDetails=true&access_token=access_token&scope=app"
+            )
+            .reply(200, {
+              data: {
+                Rosters: [
+                  {
+                    EventNumber: "0",
+                    eventdate: date,
+                    athleteid: "1",
+                    taskid: "2",
+                    rosterid: "3",
+                    TaskName: "Example Task",
+                    FirstName: "Example",
+                    LastName: "Volunteer",
+                  },
+                ],
+              },
+            });
 
-    // 401s are calculated based on response code only, so currently response data does not matter.
-    it("HTTP 401 Unauthorized, no response data", done => {
-      nock("https://api.parkrun.com")
-        .post("/user_auth.php")
-        .reply(400);
-
-      Parkrun.authSync("user", "pass")
-        .then(() => done(new Error()))
-        .catch(err => {
-          if (err instanceof UserPassError) {
+          parkrun.getRoster(0).then((data) => {
+            roster = data[0];
             done();
-          } else {
-            done(err);
-          }
+          });
         });
-    });
+      });
+      it(`getEventNumber()`, (done) => {
+        const data = roster.getEventNumber();
+        chai.expect(data).to.be.a("number");
+        chai.expect(data).to.eql(0);
+        done();
+      });
 
-    it("HTTP 401 Unauthorized, valid response data", done => {
-      nock("https://api.parkrun.com")
-        .post("/user_auth.php")
-        .reply(400, {
-          error: "invalid_grant",
-          error_description: "Invalid username and password combination"
-        });
+      it(`getEventDate()`, (done) => {
+        const data = roster.getEventDate();
+        chai.expect(data).to.be.a("date");
+        chai.expect(data).to.eql(date);
+        done();
+      });
 
-      Parkrun.authSync("user", "pass")
-        .then(() => done(new Error()))
-        .catch(err => {
-          if (err instanceof UserPassError) {
-            done();
-          } else {
-            done(err);
-          }
-        });
+      it(`getVolunteerID()`, (done) => {
+        const data = roster.getVolunteerID();
+        chai.expect(data).to.be.a("number");
+        chai.expect(data).to.eql(1);
+        done();
+      });
+
+      it(`getTaskID()`, (done) => {
+        const data = roster.getTaskID();
+        chai.expect(data).to.be.a("number");
+        chai.expect(data).to.eql(2);
+        done();
+      });
+
+      it(`getRosterID()`, (done) => {
+        const data = roster.getRosterID();
+        chai.expect(data).to.be.a("number");
+        chai.expect(data).to.eql(3);
+        done();
+      });
+
+      it(`getTaskName()`, (done) => {
+        const data = roster.getTaskName();
+        chai.expect(data).to.be.a("string");
+        chai.expect(data).to.eql("Example Task");
+        done();
+      });
+
+      it(`getVolunteerFirstName()`, (done) => {
+        const data = roster.getVolunteerFirstName();
+        chai.expect(data).to.be.a("string");
+        chai.expect(data).to.eql("Example");
+        done();
+      });
+
+      it(`getVolunteerLastName()`, (done) => {
+        const data = roster.getVolunteerLastName();
+        chai.expect(data).to.be.a("string");
+        chai.expect(data).to.eql("Volunteer");
+        done();
+      });
     });
   });
 });
