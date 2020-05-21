c016638bdb8eac12368c8629c3c47aa86f2e0c03
James Cahill <jcahill773@gmail.com>
2020-05-21 14:38:05 +0100

    ClientUser bugfix and FULL test mocking including createFreedomRun

--- tests/mock.js
+++ tests/mock.js
@@ -2,10 +2,14 @@ const Parkrun = require("../src/classes/parkrun");
 const chai = require("chai");
 const nock = require("nock");
 
+const _merge = require("lodash.merge");
+
 const ValidationError = require("../src/errors/ParkrunValidationError");
 const UserPassError = require("../src/errors/ParkrunUserPassError");
 const RefreshExpiredError = require("../src/errors/ParkrunRefreshExpiredError");
 const AuthError = require("../src/errors/ParkrunAuthError");
+const DataNotAvailableError = require("../src/errors/ParkrunDataNotAvailableError");
+const NetError = require("../src/errors/ParkrunNetError");
 
 const refresh = require("../src/common/refresh");
 
@@ -22,6 +26,26 @@ const getFakeInstance = (callback) => {
   Parkrun.authSync("user", "pass").then((parkrun) => {
     callback(parkrun);
   });
+};
+
+// Parkrun getUser() response data.
+const userResponse = {
+  data: {
+    Athletes: [
+      {
+        AthleteID: "1",
+        Avatar: "http://avatar.server.tld/avatar.ext",
+        ClubName: "Example Club",
+        CountryCode: "0", // unused
+        FirstName: "Example",
+        HomeRunID: "2",
+        HomeRunLocation: "Example Location",
+        HomeRunName: "Example parkrun",
+        LastName: "User",
+        OrganisationID: "0", // unused
+      },
+    ],
+  },
 };
 
 chai.should();
@@ -282,6 +306,195 @@ describe("Mock", () => {
             done(err);
           }
         });
+    });
+  });
+
+  describe("ClientUser", () => {
+    athlete = null;
+    DOB = new Date();
+    const staticData = _merge(userResponse, {
+      data: {
+        Athletes: [
+          {
+            ClubID: "3",
+            ConfirmCode: "unused!",
+            DOB,
+            OKtoMail: 1,
+            Postcode: "90210",
+            PreParkrunExerciseFrequency: "4",
+            WheelchairAthlete: 1,
+            eMailID: "user@mail.server.tld",
+            Sex: "M",
+          },
+        ],
+      },
+    });
+
+    before((done) => {
+      getFakeInstance((parkrun) => {
+        nock("https://api.parkrun.com")
+          .get(
+            "/v1/me?expandedDetails=true&access_token=access_token&scope=app"
+          )
+          .reply(
+            200,
+            _merge(staticData, {
+              data: {
+                Athletes: [
+                  {
+                    MobileNumber: "01632960123", // Ofcom [UK Regulator] Drama Number
+                  },
+                ],
+              },
+            })
+          );
+
+        parkrun.getMe().then((data) => {
+          athlete = data;
+          done();
+        });
+      });
+    });
+
+    it("createFreedomRun(), HTTP 200 OK, Valid Response Data", (done) => {
+      nock("https://api.parkrun.com")
+        .post("/v1/freedomruns?access_token=access_token&scope=app")
+        .reply(200, {
+          data: {
+            FreedomRuns: {
+              freedomID: 1,
+            },
+          },
+        });
+      athlete
+        .createFreedomRun("0", "2020", "01", "01", "00:30:00")
+        .then((id) => {
+          chai.expect(id).to.be.a("number");
+          chai.expect(id).to.eql(1);
+          done();
+        });
+    });
+
+    it("createFreedomRun(), HTTP 400 Bad Request", (done) => {
+      nock("https://api.parkrun.com")
+        .post("/v1/freedomruns?access_token=access_token&scope=app")
+        .reply(400);
+      athlete
+        .createFreedomRun("0", "2020", "01", "01", "00:30:00")
+        .catch((err) => {
+          if (err instanceof NetError) {
+            done();
+          } else {
+            done(err);
+          }
+        });
+    });
+
+    it("createFreedomRun(), Invalid data lengths (Year)", (done) => {
+      athlete
+        .createFreedomRun("0", "20201", "01", "01", "00:30:00")
+        .catch((err) => {
+          err instanceof Error && err.message == "Invalid input!"
+            ? done()
+            : done(err);
+        });
+    });
+
+    it("createFreedomRun(), Invalid data lengths (Month)", (done) => {
+      athlete
+        .createFreedomRun("0", "2020", "012", "01", "00:30:00")
+        .catch((err) => {
+          err instanceof Error && err.message == "Invalid input!"
+            ? done()
+            : done(err);
+        });
+    });
+
+    it("createFreedomRun(), Invalid data lengths (Day)", (done) => {
+      athlete
+        .createFreedomRun("0", "2020", "01", "012", "00:30:00")
+        .catch((err) => {
+          err instanceof Error && err.message == "Invalid input!"
+            ? done()
+            : done(err);
+        });
+    });
+
+    it("getClubID()", (done) => {
+      const data = athlete.getClubID();
+      chai.expect(data).to.be.a("number");
+      chai.expect(data).to.eql(3);
+      done();
+    });
+
+    it("getDOB()", (done) => {
+      const data = athlete.getDOB();
+      chai.expect(data).to.be.a("date");
+      chai.expect(data).to.eql(DOB);
+      done();
+    });
+
+    it("getMobileNumber() (Available)", (done) => {
+      const data = athlete.getMobileNumber();
+      chai.expect(data).to.eql("01632960123");
+      done();
+    });
+
+    it("getMobileNumber() (Not Available)", (done) => {
+      athlete2 = athlete;
+      athlete2._mobileNumber = undefined;
+      try {
+        athlete2.getMobileNumber();
+        done(new Error());
+      } catch (err) {
+        if (err instanceof DataNotAvailableError) {
+          done();
+        } else {
+          done(err);
+        }
+      }
+    });
+
+    it("getCommunicationAllowed()", (done) => {
+      const data = athlete.getCommunicationAllowed();
+      chai.expect(data).to.be.a("boolean");
+      chai.assert(data); // Assert true, other ways don't work at the moment. (May '20)
+      done();
+    });
+
+    it("getPostcode()", (done) => {
+      const data = athlete.getPostcode();
+      chai.expect(data).to.be.a("string");
+      chai.expect(data).to.eql("90210");
+      done();
+    });
+
+    it("getPreSignupExerciseFrequency()", (done) => {
+      const data = athlete.getPreSignupExerciseFrequency();
+      chai.expect(data).to.be.a("number");
+      chai.expect(data).to.eql(4);
+      done();
+    });
+
+    it("getIsWheelchairUser()", (done) => {
+      const data = athlete.getIsWheelchairUser();
+      chai.expect(data).to.be.a("boolean");
+      chai.assert(data); // Assert true, other ways don't work at the moment. (May '20)
+      done();
+    });
+
+    it("getEmail()", (done) => {
+      const data = athlete.getEmail();
+      chai.expect(data).to.be.a("string");
+      chai.expect(data).to.eql("user@mail.server.tld");
+      done();
+    });
+
+    it("getSex()", (done) => {
+      data = athlete.getSex();
+      chai.expect(data).to.be.a("string");
+      chai.expect(data).to.eql("M");
+      done();
     });
   });
 });
