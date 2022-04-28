2072f924a5c268e9e0b2de440ee08b720f7be140
James <jcahill773@gmail.com>
2022-04-28 17:18:30 +0100

    Update webpack tests conf to work in typescript

--- web-tests.webpack.config.js
+++ web-tests.webpack.config.js
@@ -5,54 +5,41 @@ const path = require("path");
 const glob = require("glob");
 
 module.exports = {
-  entry: glob.sync("./tests/*js"),
-  output: {
-    filename: "web-tests.tmp.js",
-    path: path.resolve(__dirname, "tests/platform-web"),
-    library: "ParkrunTests",
-    libraryTarget: "umd",
-  },
-  //node: {
-  //  http: true, // Polyfill stream-http as the core node 'http' module (it's the default in webpack)
-  //  fs: false
-  //},
-  externals: {
-    chai: "chai",
-    describe: "describe",
-    "../src/classes/parkrun": "Parkrun",
-  },
-  // Loaders
-  module: {
-    rules: [
-      // JavaScript/JSX Files (via Babel Loader)
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: "> 1%, not IE 11, not dead",
-                  useBuiltIns: "entry",
-                  corejs: "3.6.4",
-                  //"modules": "umd"
-                },
-              ],
-            ],
-            plugins: ["@babel/plugin-proposal-object-rest-spread"],
-          },
-        },
-      },
-    ],
-  },
-  // Plugins
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      ID: process.env.ID,
-      PASS: process.env.PASS,
-    }),
-  ],
+	entry: glob.sync("./tests/*.ts"),
+	output: {
+		filename: "web-tests.tmp.js",
+		path: path.resolve(__dirname, "tests/platform-web"),
+		library: "ParkrunTests",
+		libraryTarget: "umd",
+	},
+	externals: {
+		chai: "chai",
+		describe: "describe",
+		"../src": "Parkrun",
+	},
+	// Loaders
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: {
+					loader: "ts-loader",
+					options: {
+						configFile: "tsconfig.webpack-test.json",
+					},
+				},
+				exclude: [/node_modules/],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts"],
+	},
+	// Plugins
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			ID: process.env.ID,
+			PASS: process.env.PASS,
+		}),
+	],
 };
